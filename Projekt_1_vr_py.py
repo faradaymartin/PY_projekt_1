8513ba6cf84bd951bc107e28201e87b8b81989f3
faradaymartin <faradaymartin@gmail.com>
2024-10-30 19:00:56 +0100

    OPRAVA
    Odstranění navíc textové části (TEXTS)
    oprava --> LEN | OCCURRENCES      | NR.

--- Projekt_1_vr_py.py
+++ Projekt_1_vr_py.py
@@ -47,11 +47,7 @@ if login in uzivatel and password == uzivatel[login]:
 else:
     print("unregistered user, terminating the program..")
 
-TEXTS = [
-    '''Situated about 10 miles west of Kemmerer, Fossil Butte is a ruggedly impressive topographic feature that rises sharply some 1000 feet above Twin Creek Valley to an elevation of more than 7500 feet above sea level. The butte is located just north of US 30N and the Union Pacific Railroad, which traverse the valley.''',
-    '''At the base of Fossil Butte are the bright red, purple, yellow and gray beds of the Wasatch Formation. Eroded portions of these horizontal beds slope gradually upward from the valley floor and steepen abruptly. Overlying them and extending to the top of the butte are the much steeper buff-to-white beds of the Green River Formation, which are about 300 feet thick.''',
-    '''The monument contains 8198 acres and protects a portion of the largest deposit of freshwater fish fossils in the world. The richest fossil fish deposits are found in multiple limestone layers, which lie some 100 feet below the top of the butte. The fossils represent several varieties of perch, as well as other freshwater genera and herring similar to those in modern oceans. Other fish such as paddlefish, garpike and stingray are also present.'''
-]
+
 
 def analyze_text(text):
     words = text.split()
@@ -85,7 +81,7 @@ if vyber in range(1, 4):
     for key, value in analysis.items():
         if key == "Word length frequencies":
             print("-" * 43)
-            print("LEN | OCCURRENCES | NR.")
+            print("LEN | OCCURRENCES      | NR.")
             print("-" * 43)
             max_freq = max(value.values())
             for length, freq in sorted(value.items()):
